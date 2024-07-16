3f962c1df25a114a8b33ce37a923f4dd041cf8be
Jose-Maykol <jpaniura@unsa.edu.pe>
2024-07-16 00:49:20 -0500

    feat: add chapter creation and user chapter creation jobs

--- src/chapters/chapters.processor.ts
+++ src/chapters/chapters.processor.ts
@@ -3,26 +3,37 @@ import { InjectRepository } from '@nestjs/typeorm'
 import { Process, Processor } from '@nestjs/bull'
 import { Repository } from 'typeorm'
 import { Job } from 'bull'
+import { UserChapter } from '../models/userChapter.entity'
+import { ChaptersService } from './chapters.service'
 
 @Processor('chapters-creation')
 export class ChapterProcessor {
   constructor(
     @InjectRepository(Chapter)
     private readonly chapterRepository: Repository<Chapter>,
+    @InjectRepository(UserChapter)
+    private readonly userChapterRepository: Repository<UserChapter>,
+    private chapterService: ChaptersService,
   ) {}
 
   @Process('chapters-creation-job')
   async handleChapterCreation(job: Job<{ mangaId: string; chapters: number }>) {
     try {
       const { mangaId, chapters } = job.data
+      await this.chapterService.createChapters(mangaId, chapters)
+    } catch (error) {
+      throw error
+    }
+  }
 
-      for (let chapter = 1; chapter <= chapters; chapter++) {
-        const chapterEntity = this.chapterRepository.create({
-          manga: mangaId,
-          chapter_number: chapter,
-        })
-        await this.chapterRepository.save(chapterEntity)
-      }
+  @Process('user-chapters-creation-job')
+  async handleUserChapterCreation(
+    job: Job<{ userId: string; mangaId: string }>,
+  ) {
+    try {
+      const { userId, mangaId } = job.data
+      const chapters = await this.chapterService.getChaptersId(mangaId)
+      await this.chapterService.createUserChapters(chapters, userId)
     } catch (error) {
       throw error
     }
--- src/chapters/chapters.service.ts
+++ src/chapters/chapters.service.ts
@@ -1,22 +1,65 @@
 import { Chapter } from '../models/chapter.entity'
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
-import { CreateChapterDto } from './dto/create-chapter.dto'
+import { DataSource, Repository } from 'typeorm'
+import { UserChapter } from '../models/userChapter.entity'
 
 @Injectable()
 export class ChaptersService {
   constructor(
     @InjectRepository(Chapter)
     private readonly chapterRepository: Repository<Chapter>,
+    private readonly dataSource: DataSource,
   ) {}
 
-  async create(createChapter: CreateChapterDto): Promise<Chapter> {
-    const chapter = this.chapterRepository.create(createChapter)
-    return this.chapterRepository.save(chapter, {
-      data: {
+  async createChapters(mangaId: string, chapters: number) {
+    await this.dataSource
+      .createQueryBuilder()
+      .insert()
+      .into(Chapter)
+      .values([
+        ...Array.from({ length: chapters }).map((_, index) => ({
+          manga: {
+            id: mangaId,
+          },
+          chapter_number: index + 1,
+        })),
+      ])
+      .execute()
+  }
+
+  async createUserChapters(chapters: { mangaId: string }[], userId: string) {
+    await this.dataSource
+      .createQueryBuilder()
+      .insert()
+      .into(UserChapter)
+      .values([
+        ...chapters.map((chapter) => ({
+          user: {
+            id: userId,
+          },
+          chapter: {
+            id: chapter.mangaId,
+          },
+        })),
+      ])
+      .execute()
+  }
+
+  async getChaptersId(mangaId: string): Promise<{ mangaId: string }[]> {
+    const chapters = await this.chapterRepository.find({
+      where: {
+        manga: {
+          id: mangaId,
+        },
+      },
+      select: {
         id: true,
       },
     })
+    console.log(chapters)
+    return chapters.map((chapter) => ({
+      mangaId: chapter.id,
+    }))
   }
 }
