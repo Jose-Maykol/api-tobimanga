d90c15170b1914cc6fc0ed2896a7f6c0b0113e68
Jose-Maykol <jpaniura@unsa.edu.pe>
2024-10-27 00:50:31 -0500

    feat: add user-related database schemas

---
+++ src/modules/database/schemas/user-manga.schema.ts
@@ -0,0 +1,22 @@
+import { pgTable, smallint, timestamp, uuid } from 'drizzle-orm/pg-core'
+import { users } from './user.schema'
+import { mangas } from './manga.schema'
+import { sql } from 'drizzle-orm'
+import { readingStatusEnum } from './reading-status.schema'
+
+export const userMangas = pgTable('user_mangas', {
+  id: uuid('id')
+    .default(sql`uuid_generate_v4()`)
+    .unique()
+    .primaryKey(),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.userId, { onDelete: 'cascade' }),
+  mangaId: uuid('manga_id')
+    .notNull()
+    .references(() => mangas.mangaId, { onDelete: 'cascade' }),
+  rating: smallint('rating'),
+  readingStatus: readingStatusEnum('reading_status').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at'),
+})
