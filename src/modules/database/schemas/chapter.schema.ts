180271163c92e21cc24a5a123b8b5a6ddeee9996
Jose-Maykol <jpaniura@unsa.edu.pe>
2024-10-27 00:51:22 -0500

    feat: add database schemas for authors, chapters, demographics, and genres

---
+++ src/modules/database/schemas/chapter.schema.ts
@@ -0,0 +1,16 @@
+import { date, pgTable, smallint, timestamp, uuid } from 'drizzle-orm/pg-core'
+import { mangas } from './manga.schema'
+import { sql } from 'drizzle-orm'
+
+export const chapters = pgTable('chapters', {
+  chapterId: uuid('chapter_id')
+    .default(sql`uuid_generate_v4()`)
+    .primaryKey(),
+  mangaId: uuid('manga_id')
+    .notNull()
+    .references(() => mangas.mangaId, { onDelete: 'cascade' }),
+  chapterNumber: smallint('chapter_number').notNull(),
+  releaseDate: date('release_date'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at'),
+})
