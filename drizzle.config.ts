78a5bf1ac518679ff45c9d136749b6d4cd6aaa68
Jose-Maykol <jpaniura@unsa.edu.pe>
2024-10-27 00:46:12 -0500

    refactor: add drizzle configuration for PostgreSQL database

---
+++ drizzle.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'drizzle-kit'
+import { ConfigService } from '@nestjs/config'
+import 'dotenv/config'
+
+const configService = new ConfigService()
+
+export default defineConfig({
+  dialect: 'postgresql',
+  schema: './src/modules/database/schemas',
+  out: './src/modules/database/migrations',
+  dbCredentials: {
+    host: configService.get('POSTGRES_HOST'),
+    port: configService.get('POSTGRES_PORT'),
+    user: configService.get('POSTGRES_USER'),
+    password: configService.get('POSTGRES_PASSWORD'),
+    database: configService.get('POSTGRES_DATABASE'),
+    ssl: configService.get('POSTGRES_SSL') === 'true',
+  },
+})
