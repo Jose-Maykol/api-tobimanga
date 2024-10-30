b2899b6ecea3684ae64cb8f41b2d810852effae1
Jose-Maykol <jpaniura@unsa.edu.pe>
2024-10-30 01:06:49 -0500

    refactor: update database configuration defaults

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,11 +9,11 @@ export default defineConfig({
   schema: './src/modules/database/schemas',
   out: './src/modules/database/migrations',
   dbCredentials: {
-    host: configService.get('POSTGRES_HOST'),
-    port: configService.get('POSTGRES_PORT'),
-    user: configService.get('POSTGRES_USER'),
-    password: configService.get('POSTGRES_PASSWORD'),
-    database: configService.get('POSTGRES_DATABASE'),
+    host: configService.get('POSTGRES_HOST') || '',
+    port: configService.get('POSTGRES_PORT') || 5432,
+    user: configService.get('POSTGRES_USER') || '',
+    password: configService.get('POSTGRES_PASSWORD') || '',
+    database: configService.get('POSTGRES_DATABASE') || '',
     ssl: configService.get('POSTGRES_SSL') === 'true',
   },
 })
