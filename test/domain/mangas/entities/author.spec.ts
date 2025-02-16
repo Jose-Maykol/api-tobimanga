f2bb93aa2e473b4b9b14f8c62005adb99bf62375
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-02-16 11:38:15 -0500

    refactor: update test configurations and remove obsolete test files

---
+++ test/domain/mangas/entities/author.spec.ts
@@ -0,0 +1,62 @@
+import { InvalidAuthorException } from '@/modules/v1/manga/application/exceptions/invalid-author.exception'
+import { Author } from '@/modules/v1/manga/domain/entities/author.entity'
+import AuthorType from '@/modules/v1/manga/domain/types/author'
+
+describe('Author Entity', () => {
+  const validAuthorProps: AuthorType = {
+    id: '1',
+    name: 'Author Name',
+    createdAt: new Date(),
+    updatedAt: null,
+  }
+
+  let author: Author
+
+  beforeEach(() => {
+    author = new Author(validAuthorProps)
+  })
+
+  it('should create an author with valid properties', () => {
+    expect(author.getId()).toBe(validAuthorProps.id)
+    expect(author.getName()).toBe(validAuthorProps.name)
+    expect(author.getCreatedAt()).toBe(validAuthorProps.createdAt)
+    expect(author.getUpdatedAt()).toBeNull()
+  })
+
+  it('should update the author name', () => {
+    const newName = 'New Author Name'
+    author.setName(newName)
+    expect(author.getName()).toBe(newName)
+  })
+
+  it('should update the updatedAt property', () => {
+    const newUpdatedAt = new Date()
+    author.setUpdatedAt(newUpdatedAt)
+    expect(author.getUpdatedAt()).toBe(newUpdatedAt)
+  })
+
+  it('should throw an error if name is empty', () => {
+    const invalidName = ' '
+    expect(() => author.validateName(invalidName)).toThrow(
+      InvalidAuthorException,
+    )
+    expect(() => author.validateName(invalidName)).toThrow(
+      'El nombre del autor es requerido',
+    )
+  })
+
+  it('should throw an error if name exceeds 100 characters', () => {
+    const invalidName = 'a'.repeat(101)
+    expect(() => author.validateName(invalidName)).toThrow(
+      InvalidAuthorException,
+    )
+    expect(() => author.validateName(invalidName)).toThrow(
+      'El nombre del autor no puede exceder los 100 caracteres',
+    )
+  })
+
+  it('should not throw an error for valid name', () => {
+    const validName = 'Valid Author Name'
+    expect(() => author.validateName(validName)).not.toThrow()
+  })
+})
