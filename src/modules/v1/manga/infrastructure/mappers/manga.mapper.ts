4f5120a0060e32a5abc5c80fe22eb06e0725c4e8
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-02-19 22:41:59 -0500

    refactor: enhance manga mapping with domain conversion and persistence logic

--- src/modules/v1/manga/infrastructure/mappers/manga.mapper.ts
+++ src/modules/v1/manga/infrastructure/mappers/manga.mapper.ts
@@ -1,36 +1,91 @@
+import { mangas } from '@/modules/database/schemas/manga.schema'
 import { Manga } from '../../domain/entities/manga.entity'
+import MangaRecord from '../../domain/types/manga'
+import { InferInsertModel } from 'drizzle-orm'
+import { genres } from '@/modules/database/schemas/genres.schema'
+import { authors } from '@/modules/database/schemas/author.schema'
+import { Genre } from '../../domain/entities/genre.entity'
+import { Author } from '../../domain/entities/author.entity'
 
-/* type MangaPersistence = {
-    id: string;
-  originalName: string
-  alternativeNames: string[]
-  sinopsis: string
-  chapters: number
-  releaseDate: Date
-  coverImage: string
-  bannerImage: string
-  publicationStatus: string
-  rating: number
-  active: boolean
-  authors: string[]
-  genres: string[]
-  demographic: string
-  createdAt: Date
-  updatedAt: Date
-}
- */
 export class MangaMapper {
-  static toPersistence(manga: Manga): any {
+  static toDomain(manga: MangaRecord): Manga {
+    const newManga = new Manga({
+      originalName: manga.originalName,
+      alternativeNames: manga.alternativeNames,
+      sinopsis: manga.sinopsis,
+      chapters: manga.chapters,
+      releaseDate: manga.releaseDate,
+      coverImage: manga.coverImage,
+      bannerImage: manga.bannerImage,
+      publicationStatus: manga.publicationStatus,
+      id: manga.id,
+      rating: manga.rating,
+      active: manga.active,
+      slugName: manga.slugName,
+      scrappingName: manga.scrappingName,
+      createdAt: manga.createdAt,
+      updatedAt: manga.updatedAt,
+    })
+
+    newManga.genres = manga.genres.map(
+      (genre) =>
+        new Genre({
+          id: genre.id,
+          name: genre.name,
+          createdAt: genre.createdAt,
+          updatedAt: genre.updatedAt,
+        }),
+    )
+
+    newManga.authors = manga.authors.map(
+      (author) =>
+        new Author({
+          id: author.id,
+          name: author.name,
+          createdAt: author.createdAt,
+          updatedAt: author.updatedAt,
+        }),
+    )
+
+    return newManga
+  }
+
+  static toPersistence(manga: Manga): {
+    manga: InferInsertModel<typeof mangas>
+    genres: InferInsertModel<typeof genres>[]
+    authors: InferInsertModel<typeof authors>[]
+  } {
     return {
-      originalName: manga.getOriginalName(),
-      alternativeNames: manga.getAlternativeNames(),
-      sinopsis: manga.getSinopsis(),
-      chapters: manga.getChapters(),
-      releaseDate: manga.getReleaseDate().toISOString(),
-      coverImage: manga.getCoverImage(),
-      bannerImage: manga.getBannerImage(),
-      publicationStatus: manga.getPublicationStatus(),
-      demographic: manga.getDemographic().getId(),
+      manga: {
+        id: manga.id,
+        originalName: manga.originalName,
+        alternativeNames: manga.alternativeNames,
+        sinopsis: manga.sinopsis,
+        chapters: manga.chapters,
+        releaseDate: manga.releaseDate.toISOString(),
+        coverImage: manga.coverImage,
+        bannerImage: manga.bannerImage,
+        publicationStatus: manga.publicationStatus as any,
+        rating: manga.rating,
+        active: manga.active,
+        slugName: manga.slugName,
+        scrappingName: manga.scrappingName,
+        createdAt: manga.createdAt,
+        updatedAt: manga.updatedAt,
+        demographicId: manga.demographic.getId(),
+      },
+      authors: manga.authors.map((author) => ({
+        id: author.getId(),
+        name: author.getName(),
+        createdAt: author.getCreatedAt(),
+        updatedAt: author.getUpdatedAt(),
+      })),
+      genres: manga.genres.map((genre) => ({
+        id: genre.getId(),
+        name: genre.getName(),
+        createdAt: genre.getCreatedAt(),
+        updatedAt: genre.getUpdatedAt(),
+      })),
     }
   }
 }
