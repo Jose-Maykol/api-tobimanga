aa4283dd37fe767ed0e7fc3a45b98d77d42dc735
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-03-27 22:01:47 -0500

    feat: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as bodyParser from 'body-parser'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Tobimanga API')
+    .setDescription('The Tobimanga API description')
+    .setVersion('1.0')
+    .addTag('tobimanga')
+    /* .addBearerAuth() */
+    .build()
+
   app.use(bodyParser.json({ limit: '5mb' }))
   app.setGlobalPrefix('api')
   app.enableCors()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api/docs', app, documentFactory())
+
   await app.listen(8000)
 }
 bootstrap()
