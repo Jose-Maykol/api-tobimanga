80836359ba82af20677cb0214fb2ea676a06b460
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-05-25 01:25:32 -0500

    feat: define user entity with necessary fields

---
+++ src/modules/v2/user/domain/entities/user.ts
@@ -0,0 +1,13 @@
+export type User = {
+  id: string
+  username: string
+  password: string
+  email: string
+  profileImage: string | null
+  coverImage: string | null
+  role: 'USER' | 'ADMIN'
+  isActive: boolean
+  refreshToken: string | null
+  createdAt: Date
+  updatedAt: Date | null
+}
