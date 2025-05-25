52cfc608360b88c1d024839727e260e1d3f43a6b
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-05-25 01:23:34 -0500

    feat: implement JWT strategy for access and refresh token validation

---
+++ src/modules/v2/auth/application/strategies/jwt-refresh.strategy.ts
@@ -0,0 +1,51 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { ConfigService } from '@nestjs/config'
+import { Request } from 'express'
+import { JwtPayload } from '../../domain/interfaces/auth.interface'
+import { AuthService } from '../services/auth.service'
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(
+    private configService: ConfigService,
+    private authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromBodyField('refresh_token'),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('REFRESH_SECRET_KEY'),
+      passReqToCallback: true,
+    })
+  }
+
+  async validate(req: Request, payload: JwtPayload) {
+    if (payload.type !== 'refresh') {
+      throw new UnauthorizedException('Token inválido')
+    }
+
+    const refreshToken = req.body?.refresh_token
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token requerido')
+    }
+
+    /* const isValidToken = await this.authService.validateRefreshToken(
+      payload.sub,
+      refreshToken,
+    )
+    if (!isValidToken) {
+      throw new UnauthorizedException('Refresh token inválido')
+    } */
+
+    /* const user = await this.usersService.findById(payload.sub)
+    return {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    } */
+  }
+}
---
+++ src/modules/v2/auth/application/strategies/jwt.strategy.ts
@@ -0,0 +1,41 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { AuthenticatedUser } from '@/common/interfaces/authenticated-user.interface'
+import { DecodedJwtPayload } from '../../domain/interfaces/auth.interface'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('SECRET_KEY'),
+    })
+  }
+
+  async validate(payload: DecodedJwtPayload): Promise<AuthenticatedUser> {
+    const { email, sub } = payload
+
+    if (payload.type !== 'access') {
+      throw new UnauthorizedException('Token inválido')
+    }
+
+    if (!email || !sub) {
+      throw new UnauthorizedException('Token inválido')
+    }
+
+    /* const query = new CheckUserExistsQuery(sub)
+    const user = await this.queryBus.execute(query)
+
+    if (!user) {
+      throw new UnauthorizedException('Usuario no encontrado')
+    } */
+
+    return {
+      email: payload.email,
+      id: payload.sub,
+    }
+  }
+}
