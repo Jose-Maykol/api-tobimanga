56e02367e3305ce03bfd409c53df8f88ff8cdf1d
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-06-24 23:29:11 -0500

    refactor: implement access token service

---
+++ src/infrastructure/services/access-token.service.impl.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
+import { AccessTokenService } from '@/domain/services/access-token.service'
+import { JwtPayload } from '@/domain/interfaces/auth.interface'
+
+@Injectable()
+export class AccessTokenServiceImpl implements AccessTokenService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async generateToken(payload: JwtPayload): Promise<string> {
+    return this.jwtService.signAsync(payload, {
+      secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+      expiresIn: this.configService.get<string>('JWT_ACCESS_EXPIRATION_TIME'),
+    })
+  }
+
+  async verifyToken(token: string): Promise<JwtPayload> {
+    return this.jwtService.verifyAsync<JwtPayload>(token, {
+      secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+    })
+  }
+}
