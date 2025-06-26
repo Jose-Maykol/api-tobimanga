ca42d26c22758721e8bc12df5a8d4780dc8d6305
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-06-26 00:36:34 -0500

    chore(database): modify folder structure for database module

---
+++ src/core/database/schemas/index.ts
@@ -0,0 +1,29 @@
+import { authors } from './author.schema'
+import { chapters } from './chapter.schema'
+import { demographics } from './demographic.schema'
+import { genres } from './genres.schema'
+import { mangaAuthors } from './manga-author.schema'
+import { mangaGenres } from './manga-genre.schema'
+import { mangas } from './manga.schema'
+import { publicationStatusEnum } from './publication-status.schema'
+import { readingStatusEnum } from './reading-status.schema'
+import { userChapters } from './user-chapter.schema'
+import { userFavoriteMangas } from './user-favorite-manga.schema'
+import { userMangas } from './user-manga.schema'
+import { users } from './user.schema'
+
+export const databaseSchema = {
+  authors,
+  chapters,
+  demographics,
+  genres,
+  mangaAuthors,
+  mangaGenres,
+  mangas,
+  publicationStatusEnum,
+  readingStatusEnum,
+  userChapters,
+  userFavoriteMangas,
+  userMangas,
+  users,
+}
