4d8c182508b9dfc6af5acf2d2d625006456d9032
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-07-15 22:56:22 -0500

    feat(demographic): add demographic not found exception and error codes enum

---
+++ src/modules/demographic/domain/exceptions/demographic-not-found.exception.ts
@@ -0,0 +1,11 @@
+import { BaseException } from '@/common/exceptions/base.exception'
+import { ErrorCodes } from './error-codes.enum'
+
+export class DemographicNotFoundException extends BaseException {
+  constructor(name: string) {
+    super(
+      `No se encontró la demografía ${name}`,
+      ErrorCodes.DEMOGRAPHIC_NOT_FOUND,
+    )
+  }
+}
