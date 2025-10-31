dc29e16d9865f10dd73df4b9b2f4c67d8c874f28
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-10-30 23:02:11 -0500

    feat(infrastructure): define repository tokens for dependency injection

--- src/infrastructure/infraestructure.module.ts
+++ src/infrastructure/infraestructure.module.ts
@@ -1,19 +1,40 @@
 import { Module } from '@nestjs/common'
 
 import { ChapterRepositoryImpl } from './repositories/chapter.repository.impl'
+import { DemographicRepositoryImpl } from './repositories/demographic.repository.impl'
+import { GenreRepositoryImpl } from './repositories/genre.repository.impl'
 import { MangaRepositoryImpl } from './repositories/manga.repository.impl'
+import {
+  CHAPTER_REPOSITORY,
+  DEMOGRAPHIC_REPOSITORY,
+  GENRE_REPOSITORY,
+  MANGA_REPOSITORY,
+} from './tokens/repositories'
 
 @Module({
   providers: [
     {
-      provide: 'MangaRepository',
+      provide: MANGA_REPOSITORY,
       useClass: MangaRepositoryImpl,
     },
     {
-      provide: 'ChapterRepository',
+      provide: CHAPTER_REPOSITORY,
       useClass: ChapterRepositoryImpl,
     },
+    {
+      provide: GENRE_REPOSITORY,
+      useClass: GenreRepositoryImpl,
+    },
+    {
+      provide: DEMOGRAPHIC_REPOSITORY,
+      useClass: DemographicRepositoryImpl,
+    },
   ],
-  exports: ['MangaRepository', 'ChapterRepository'],
+  exports: [
+    MANGA_REPOSITORY,
+    CHAPTER_REPOSITORY,
+    GENRE_REPOSITORY,
+    DEMOGRAPHIC_REPOSITORY,
+  ],
 })
 export class InfrastructureModule {}
---
+++ src/infrastructure/tokens/repositories.ts
@@ -0,0 +1,4 @@
+export const MANGA_REPOSITORY = 'MangaRepository'
+export const CHAPTER_REPOSITORY = 'ChapterRepository'
+export const GENRE_REPOSITORY = 'GenreRepository'
+export const DEMOGRAPHIC_REPOSITORY = 'DemographicRepository'
