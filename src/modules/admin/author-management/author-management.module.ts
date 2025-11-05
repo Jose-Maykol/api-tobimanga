49f867a190f3bd1162e70e1a65a1f5d1ef0697da
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-11-04 22:25:08 -0500

    fix: update import path

--- src/modules/admin/author-management/author-management.module.ts
+++ src/modules/admin/author-management/author-management.module.ts
@@ -7,7 +7,7 @@ import { AuthModule } from '../../auth/auth.module'
 import { CreateAuthorUseCase } from './application/use-cases/create-author.use-case'
 import { GetAllAuthorsUseCase } from './application/use-cases/get-all-authors.use-case'
 import { GetAuthorByIdUseCase } from './application/use-cases/get-author-by-id.use-case'
-import { AuthorManagementController } from './controllers/author-management.controller'
+import { AuthorManagementController } from './interface/controllers/author-management.controller'
 
 @Module({
   imports: [InfrastructureModule, DatabaseModule, AuthModule],
