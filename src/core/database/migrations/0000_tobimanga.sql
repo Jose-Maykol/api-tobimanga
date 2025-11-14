9566d65555073a9c159788dea891597497790796
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-11-14 00:09:18 -0500

    feat(database): update manga and user chapter progress schemas, add uploads table

--- src/core/database/migrations/0000_tobimanga.sql
+++ src/core/database/migrations/0000_tobimanga.sql
@@ -74,8 +74,10 @@ CREATE TABLE "mangas" (
 	"sinopsis" text NOT NULL,
 	"chapters" smallint NOT NULL,
 	"release_date" date,
-	"cover_image" text,
-	"banner_image" text,
+	"cover_image_url" text,
+	"cover_image_object_key" text,
+	"banner_image_url" text,
+	"banner_image_object_key" text,
 	"publication_status" "publication_status" NOT NULL,
 	"rating" smallint DEFAULT 0 NOT NULL,
 	"active" boolean DEFAULT true NOT NULL,
@@ -86,14 +88,21 @@ CREATE TABLE "mangas" (
 	CONSTRAINT "mangas_scrapping_name_unique" UNIQUE("scrapping_name")
 );
 --> statement-breakpoint
-CREATE TABLE "user_chapters" (
+CREATE TABLE "uploads" (
+	"upload_id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+	"content_type" text NOT NULL,
+	"url" text NOT NULL,
+	"status" text NOT NULL,
+	"object_key" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "user_chapter_progress" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"user_id" uuid NOT NULL,
 	"chapter_id" uuid NOT NULL,
-	"read" boolean DEFAULT false NOT NULL,
-	"read_at" timestamp,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp
+	"read_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "user_favorite_mangas" (
@@ -137,8 +146,8 @@ ALTER TABLE "manga_authors" ADD CONSTRAINT "manga_authors_author_id_authors_auth
 ALTER TABLE "manga_genres" ADD CONSTRAINT "manga_genres_id_mangas_manga_id_fk" FOREIGN KEY ("id") REFERENCES "public"."mangas"("manga_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "manga_genres" ADD CONSTRAINT "manga_genres_genre_id_genres_genre_id_fk" FOREIGN KEY ("genre_id") REFERENCES "public"."genres"("genre_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "mangas" ADD CONSTRAINT "mangas_demographic_id_demographics_demographic_id_fk" FOREIGN KEY ("demographic_id") REFERENCES "public"."demographics"("demographic_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "user_chapters" ADD CONSTRAINT "user_chapters_user_id_users_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("user_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "user_chapters" ADD CONSTRAINT "user_chapters_chapter_id_chapters_chapter_id_fk" FOREIGN KEY ("chapter_id") REFERENCES "public"."chapters"("chapter_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_chapter_progress" ADD CONSTRAINT "user_chapter_progress_user_id_users_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("user_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_chapter_progress" ADD CONSTRAINT "user_chapter_progress_chapter_id_chapters_chapter_id_fk" FOREIGN KEY ("chapter_id") REFERENCES "public"."chapters"("chapter_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_favorite_mangas" ADD CONSTRAINT "user_favorite_mangas_user_id_users_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("user_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_favorite_mangas" ADD CONSTRAINT "user_favorite_mangas_manga_id_mangas_manga_id_fk" FOREIGN KEY ("manga_id") REFERENCES "public"."mangas"("manga_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_mangas" ADD CONSTRAINT "user_mangas_user_id_users_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("user_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
