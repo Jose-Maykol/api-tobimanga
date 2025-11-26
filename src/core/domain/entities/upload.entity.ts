17fa5e7a8a008e9bb258d2d6411f47523eca7686
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-11-25 23:05:54 -0500

    feat(upload): update Upload interface to use UploadStatus enum for status field

--- src/core/domain/entities/upload.entity.ts
+++ src/core/domain/entities/upload.entity.ts
@@ -1,9 +1,18 @@
+export enum UploadStatus {
+  PENDING = 'PENDING',
+  ACTIVE = 'ACTIVE',
+  DELETED = 'DELETED',
+}
+
 export interface Upload {
   id: string
+  fileName: string
   contentType: string
   url: string
-  status: 'USED' | 'TEMPORARY'
+  status: UploadStatus
   objectKey: string
+  entityType?: string | null
+  usedAt?: Date | null
   createdAt: Date
   updatedAt?: Date | null
 }
