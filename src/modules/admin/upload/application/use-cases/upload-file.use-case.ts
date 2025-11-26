e49e3901a56a5bc83ed8b44a3d3891dd1db34df0
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-11-25 23:07:59 -0500

    feat(upload): refactor execute method to accept params and update upload status

--- src/modules/admin/upload/application/use-cases/upload-file.use-case.ts
+++ src/modules/admin/upload/application/use-cases/upload-file.use-case.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common'
 
-import { Upload } from '@/core/domain/entities/upload.entity'
+import { Upload, UploadStatus } from '@/core/domain/entities/upload.entity'
 import { UploadRepository } from '@/core/domain/repositories/upload.repository'
 import { IMAGE_STORAGE_SERVICE } from '@/core/storage/constants/storage.constants'
 import { StorageService } from '@/core/storage/interfaces/storage.service'
@@ -16,22 +16,26 @@ export class UploadFileUseCase {
 
   /**
    * Uploads a file, persists its metadata, and returns its storage data.
-   * @param file File to upload
+   * @param params Object containing file and entityType
    * @returns Object with url, contentType, and objectKey
    */
-  async execute(
-    file: Express.Multer.File,
-  ): Promise<{ url: string; contentType: string; objectKey: string }> {
+  async execute(params: {
+    file: Express.Multer.File
+    entityType?: string
+  }): Promise<{ url: string; contentType: string; objectKey: string }> {
+    const { file, entityType } = params
     const uploadResult = await this.imageStorageService.uploadFromBuffer(
       file.buffer,
     )
 
     const upload: Upload = {
       id: crypto.randomUUID(),
+      fileName: file.originalname,
       contentType: file.mimetype,
       url: uploadResult.url,
-      status: 'USED',
+      status: UploadStatus.PENDING,
       objectKey: uploadResult.objectKey,
+      entityType: entityType || null,
       createdAt: new Date(),
       updatedAt: new Date(),
     }
