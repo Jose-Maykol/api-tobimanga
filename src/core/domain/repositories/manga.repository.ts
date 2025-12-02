de565689845b8396b4472d489c8add887472895d
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-12-02 00:09:31 -0500

    feat(manga): implement pagination and count methods in MangaRepository

--- src/core/domain/repositories/manga.repository.ts
+++ src/core/domain/repositories/manga.repository.ts
@@ -5,5 +5,7 @@ export interface MangaRepository {
   findById(id: string): Promise<Manga | null>
   findAll(): Promise<Manga[]> */
   save(manga: Manga): Promise<Manga>
+  findAll(page: number, limit: number): Promise<Manga[]>
+  countAll(): Promise<number>
   existBySlugName(slugName: string): Promise<boolean>
 }
--- src/infrastructure/repositories/manga.repository.impl.ts
+++ src/infrastructure/repositories/manga.repository.impl.ts
@@ -1,14 +1,16 @@
-import { eq } from 'drizzle-orm'
+import { count, eq } from 'drizzle-orm'
 
 import { Inject, Injectable } from '@nestjs/common'
 
 import { DATABASE_SERVICE } from '@/core/database/constants/database.constants'
+import { demographics } from '@/core/database/schemas/demographic.schema'
 import { mangas } from '@/core/database/schemas/manga.schema'
 import { mangaAuthors } from '@/core/database/schemas/manga-author.schema'
 import { mangaGenres } from '@/core/database/schemas/manga-genre.schema'
 import { DatabaseService } from '@/core/database/services/database.service'
 import { Manga } from '@/core/domain/entities/manga.entity'
 import { MangaRepository } from '@/core/domain/repositories/manga.repository'
+import { PublicationStatus } from '@/modules/manga/application/enums/publication-status.enum'
 
 @Injectable()
 export class MangaRepositoryImpl implements MangaRepository {
@@ -27,7 +29,7 @@ export class MangaRepositoryImpl implements MangaRepository {
       alternativeNames: manga.alternativeNames,
       sinopsis: manga.sinopsis,
       chapters: manga.chapters,
-      releaseDate: manga.releaseDate ? manga.releaseDate.toISOString() : null,
+      releaseDate: manga.releaseDate.toISOString(),
       coverImageUrl: manga.coverImage,
       bannerImageUrl: manga.bannerImage,
       publicationStatus: manga.publicationStatus,
@@ -82,5 +84,49 @@ export class MangaRepositoryImpl implements MangaRepository {
       .where(eq(mangas.slugName, slugName))
       .limit(1)
     return result.length > 0
+  }
+
+  async findAll(page: number, limit: number): Promise<Manga[]> {
+    const offset = (page - 1) * limit
+
+    const results = await this.db.client
+      .select({
+        manga: mangas,
+        authors: mangaAuthors,
+        genres: mangaGenres,
+        demographic: demographics,
+      })
+      .from(mangas)
+      .innerJoin(mangaAuthors, eq(mangas.id, mangaAuthors.mangaId))
+      .innerJoin(mangaGenres, eq(mangas.id, mangaGenres.mangaId))
+      .innerJoin(demographics, eq(mangas.demographicId, demographics.id))
+      .limit(limit)
+      .offset(offset)
+
+    return results.map((result) => ({
+      id: result.manga.id,
+      originalName: result.manga.originalName,
+      slugName: result.manga.slugName,
+      scrappingName: result.manga.scrappingName,
+      alternativeNames: result.manga.alternativeNames,
+      sinopsis: result.manga.sinopsis,
+      chapters: result.manga.chapters,
+      releaseDate: new Date(result.manga.releaseDate),
+      coverImage: result.manga.coverImageUrl,
+      bannerImage: result.manga.bannerImageUrl,
+      publicationStatus: result.manga.publicationStatus as PublicationStatus,
+      rating: result.manga.rating,
+      active: result.manga.active,
+      authors: [],
+      genres: [],
+      demographic: result.demographic,
+      createdAt: result.manga.createdAt,
+      updatedAt: result.manga.updatedAt,
+    }))
+  }
+
+  async countAll(): Promise<number> {
+    const result = await this.db.client.select({ total: count() }).from(mangas)
+    return result[0].total
   }
 }
