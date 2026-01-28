13f492499fe200881d69c9978f0eb9f0ba14811a
Jose-Maykol <jpaniura@unsa.edu.pe>
2026-01-27 23:11:05 -0500

    refactor(user): format user schema for improved readability

--- src/core/database/schemas/user.schema.ts
+++ src/core/database/schemas/user.schema.ts
@@ -1,4 +1,5 @@
 import { sql } from 'drizzle-orm'
+import { index } from 'drizzle-orm/pg-core'
 import {
   boolean,
   pgEnum,
@@ -11,19 +12,28 @@ import {
 
 export const userRoleEnum = pgEnum('user_role', ['USER', 'ADMIN'])
 
-export const users = pgTable('users', {
-  id: uuid('user_id')
-    .default(sql`uuid_generate_v4()`)
-    .unique()
-    .primaryKey(),
-  username: varchar('username', { length: 100 }).notNull().unique(),
-  password: varchar('password', { length: 100 }).notNull(),
-  email: varchar('email', { length: 255 }).notNull().unique(),
-  profileImage: text('profile_image'),
-  coverImage: text('cover_image'),
-  role: userRoleEnum('role').default('USER').notNull(),
-  isActive: boolean('is_active').default(true).notNull(),
-  refreshToken: text('refresh_token'),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
-})
+export const users = pgTable(
+  'users',
+  {
+    id: uuid('user_id')
+      .default(sql`uuid_generate_v4()`)
+      .unique()
+      .primaryKey(),
+    username: varchar('username', { length: 100 }).notNull().unique(),
+    password: varchar('password', { length: 100 }).notNull(),
+    email: varchar('email', { length: 255 }).notNull().unique(),
+    profileImage: text('profile_image'),
+    coverImage: text('cover_image'),
+    role: userRoleEnum('role').default('USER').notNull(),
+    isActive: boolean('is_active').default(true).notNull(),
+    refreshToken: text('refresh_token'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+  },
+  (table) => {
+    return {
+      createdAtIndex: index('users_created_at_idx').on(table.createdAt),
+      roleIndex: index('users_role_idx').on(table.role),
+    }
+  },
+)
