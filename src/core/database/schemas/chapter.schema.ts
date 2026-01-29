fa49efb5a23d5c1508e829ac4126809526e76b94
Jose-Maykol <jpaniura@unsa.edu.pe>
2026-01-28 23:27:55 -0500

    refactor(chapter): improve formatting and structure of chapter schema definition

--- src/core/database/schemas/chapter.schema.ts
+++ src/core/database/schemas/chapter.schema.ts
@@ -1,17 +1,35 @@
 import { sql } from 'drizzle-orm'
-import { date, pgTable, smallint, timestamp, uuid } from 'drizzle-orm/pg-core'
+import {
+  date,
+  index,
+  pgTable,
+  smallint,
+  timestamp,
+  uuid,
+} from 'drizzle-orm/pg-core'
 
 import { mangas } from './manga.schema'
 
-export const chapters = pgTable('chapters', {
-  id: uuid('chapter_id')
-    .default(sql`uuid_generate_v4()`)
-    .primaryKey(),
-  mangaId: uuid('manga_id')
-    .notNull()
-    .references(() => mangas.id, { onDelete: 'cascade' }),
-  chapterNumber: smallint('chapter_number').notNull(),
-  releaseDate: date('release_date'),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
-})
+export const chapters = pgTable(
+  'chapters',
+  {
+    id: uuid('chapter_id')
+      .default(sql`uuid_generate_v4()`)
+      .primaryKey(),
+    mangaId: uuid('manga_id')
+      .notNull()
+      .references(() => mangas.id, { onDelete: 'cascade' }),
+    chapterNumber: smallint('chapter_number').notNull(),
+    releaseDate: date('release_date'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+  },
+  (table) => {
+    return {
+      chapterMangaIndex: index('chapters_manga_id_idx').on(table.mangaId),
+      chapterNumberIndex: index('chapters_chapter_number_idx').on(
+        table.chapterNumber,
+      ),
+    }
+  },
+)
