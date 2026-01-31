cd55bab7d11955c1ba29bd870c07fbc0c9da3928
Jose-Maykol <jpaniura@unsa.edu.pe>
2026-01-30 23:34:13 -0500

    refactor(cron-job): update schema to use jsonb for options and improve structure

--- src/core/database/schemas/cron-job.schema.ts
+++ src/core/database/schemas/cron-job.schema.ts
@@ -1,8 +1,8 @@
 import { sql } from 'drizzle-orm'
 import {
   boolean,
+  jsonb,
   pgTable,
-  text,
   timestamp,
   uuid,
   varchar,
@@ -12,10 +12,13 @@ export const cronJobs = pgTable('cron_jobs', {
   id: uuid('cron_job_id')
     .default(sql`uuid_generate_v4()`)
     .primaryKey(),
+  key: varchar('key').notNull().unique(),
   name: varchar('name').notNull(),
   schedule: varchar('schedule').notNull(),
-  task: text('task').notNull(),
+  options: jsonb('options').notNull().default('{}'),
   isActive: boolean('is_active').notNull().default(true),
+  lastRunAt: timestamp('last_run_at'),
+  nextRunAt: timestamp('next_run_at'),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
 })
